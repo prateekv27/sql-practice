b7365b87d85a5c90469abd9833f6bb9c2ebcfe87
prateekv27 <prateekv887@gmail.com>
2026-08-03 08:34:52 +0530

    day 1 sqlpractice

---
+++ sql_PRACTICE.sql
@@ -0,0 +1,12 @@
+
+USE sql_practice;
+
+
+
+
+INSERT INTO users (name, email, gender, date_of_birth)
+VALUES
+('Prateek', 'prateek@gmail.com', 'male', '2007-05-27'),
+('Rahul', 'rahul@gmail.com', 'male', '2006-08-12');
+
+SELECT * from users;
